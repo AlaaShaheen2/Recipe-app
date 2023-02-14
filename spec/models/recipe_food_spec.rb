c0b32142993a680ba4f713d115fe04d86d70920a
luccamicale <luccamicale2015@gmail.com>
2023-02-14 14:35:26 -0300

    I created the rspec for the models

--- spec/models/recipe_food_spec.rb
+++ spec/models/recipe_food_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe RecipeFood, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @first_recipe = Food.new
+  end
+
+  describe 'When the user model is tested' do
+    it 'validates the name of the food' do
+      @first_recipe.quantity = '100g'
+      expect(@first_recipe.quantity).to eq '100g'
+    end
+    it 'validates the name of the food' do
+      @first_recipe.created_at = '2023-02-10 03:46:44.223198'
+      expect(@first_recipe.created_at).to eq '2023-02-10 03:46:44.223198'
+    end
+    it 'validates the name of the food' do
+      @first_recipe.updated_at = '2023-02-10 03:46:44.223198'
+      expect(@first_recipe.updated_at).to eq '2023-02-10 03:46:44.223198'
+    end
+  end
 end
--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Recipe, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @recipe = Recipe.new
+  end
+
+  describe 'When the recipes model is tested' do
+    it 'validates the recipe object' do
+      @recipe.name = 'Cookie'
+      expect(@recipe.name).to eq 'Cookie'
+    end
+    it 'validates the recipe object' do
+      @recipe.description = 'Chocolate with cream'
+      expect(@recipe.description).to eq 'Chocolate with cream'
+    end
+    it 'validates the recipe object' do
+      @recipe.preparation_time = 10
+      expect(@recipe.preparation_time).to eq 10
+    end
+  end
 end
