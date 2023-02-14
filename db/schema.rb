62edd66bce9b4b304c58b55e0dc71946f08bcb62
AlaaShahen2 <alaa.m.shaheen22@gmail.com>
2023-02-14 18:23:27 +0430

    Remove non nessesary columns

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_13_212021) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_14_134945) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,15 +19,12 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_13_212021) do
     t.string "measurent_unit"
     t.decimal "price"
     t.string "quantity"
-    t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "recipe_foods", force: :cascade do |t|
     t.string "quantity"
-    t.integer "recipe_id"
-    t.integer "food_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -38,7 +35,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_13_212021) do
     t.string "cooking_time"
     t.text "description"
     t.boolean "public"
-    t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
