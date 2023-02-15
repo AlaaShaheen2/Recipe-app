8e99f7267bea681cf2ee739d41d67af1617dc2d2
AlaaShaheen2 <alaa.m.shaheen22@gmail.com>
2023-02-15 16:08:31 +0430

    Update modals validations

--- app/models/food.rb
+++ app/models/food.rb
@@ -2,6 +2,8 @@ class Food < ApplicationRecord
   has_many :recipe_foods, class_name: 'RecipeFood', foreign_key: 'food_id'
   belongs_to :user, class_name: 'User', foreign_key: 'user_id'
 
-  validates :name, length: { maximum: 80 }, presence: true
+  validates :name, presence: true, length: { in: 2..250 }
+  validates :measurement_unit, presence: true
   validates :price, presence: true
+  validates :quantity, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ApplicationRecord
   has_many :foods, class_name: 'Food', foreign_key: 'user_id'
   has_many :recipes, class_name: 'Recipe', foreign_key: 'user_id'
 
-  validates :name, length: { maximum: 80 }, presence: true
+  validates :name, presence: true, length: { in: 2..250 }
 end
