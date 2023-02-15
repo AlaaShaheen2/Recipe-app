2ed03f2ddb1995268615333b7ae06532d8f79d7b
AlaaShaheen2 <alaa.m.shaheen22@gmail.com>
2023-02-15 19:38:20 +0430

    refactore

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
+  
   resources :recipes, only: [:index] do
   end
-  resources :recipe_foods
-  resources :foods
-  resources :users
+  # resources :recipe_foods
+  # resources :foods
+  # resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
