7c57c9ecbd09b7237c8c0e38b986fb6f89a50130
AlaaShaheen2 <alaa.m.shaheen22@gmail.com>
2023-02-15 16:04:42 +0430

    Update modal association

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,6 +1,6 @@
 class Food < ApplicationRecord
-  belongs_to :user, foreign_key: :user_id
-  has_many :recipe_foods, foreign_key: :food_id
+  has_many :recipe_foods, class_name: 'RecipeFood', foreign_key: 'food_id'
+  belongs_to :user, class_name: 'User', foreign_key: 'user_id'
 
   validates :name, length: { maximum: 80 }, presence: true
   validates :price, presence: true
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,6 +1,6 @@
 class Recipe < ApplicationRecord
-  belongs_to :user, foreign_key: :user_id
-  has_many :recipe_foods, foreign_key: :recipe_id
+  belongs_to :user, class_name: 'User', foreign_key: 'user_id'
+  has_many :recipe_foods, class_name: 'RecipeFood', foreign_key: 'recipe_id'
 
   validates :name, length: { maximum: 100 }, presence: true
   validates :public, presence: true
