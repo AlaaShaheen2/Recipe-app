5251525a3f78214e9fad221711fd8390e8181b94
luccamicale <luccamicale2015@gmail.com>
2023-02-17 18:23:14 -0300

    fix the problem with shopping and add more styles

--- app/controllers/shopinglists_controller.rb
+++ app/controllers/shopinglists_controller.rb
@@ -1,5 +1,5 @@
 class ShopinglistsController < ApplicationController
   def index
-    @recipefoods = RecipeFoods.includes(:food).where(recipe_id: params[:recipe_id])
+    @recipefoods = RecipeFood.includes(:food).where(recipe_id: params[:recipe_id])
   end
 end
