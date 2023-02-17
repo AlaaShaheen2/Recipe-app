66a28cc5d3ab3ed55f9feabf2e7bd0cd3242fc64
AlaaShaheen2 <alaa.m.shaheen22@gmail.com>
2023-02-17 17:10:39 +0430

    update recipe foods model

--- app/models/recipe_food.rb
+++ app/models/recipe_food.rb
@@ -1,6 +1,6 @@
 class RecipeFood < ApplicationRecord
-  belongs_to :food, dependent: :delete_all
-  belongs_to :recipe, dependent: :delete_all
+  belongs_to :food
+  belongs_to :recipe
 
   validates :quantity, presence: true
 end
