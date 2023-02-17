e223d1a51d79bb54cbddb8bac1f48ad87fcba58a
luccamicale <luccamicale2015@gmail.com>
2023-02-17 12:22:03 -0300

    fix the problem with the view shopping list

--- app/controllers/shopinglists_controller.rb
+++ app/controllers/shopinglists_controller.rb
@@ -1,5 +1,5 @@
 class ShopinglistsController < ApplicationController
   def index
-    @recipefoods = RecipeFood.includes(:food).where(recipe_id: params[:recipe_id])
+    @recipefoods = RecipeFoods.includes(:food).where(recipe_id: params[:recipe_id])
   end
 end
