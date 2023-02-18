32036917a4b9b49f3205bdfd5b29660d62a044bd
AlaaShaheen2 <alaa.m.shaheen22@gmail.com>
2023-02-18 22:48:31 +0430

    Fix specs for the project

--- spec/models/recipe_food_spec.rb
+++ spec/models/recipe_food_spec.rb
@@ -7,10 +7,6 @@ RSpec.describe RecipeFood, type: :model do
 
   describe 'When the user model is tested' do
     it 'validates the name of the food' do
-      @first_recipe.quantity = '100g'
-      expect(@first_recipe.quantity).to eq '100g'
-    end
-    it 'validates the name of the food' do
       @first_recipe.created_at = '2023-02-10 03:46:44.223198'
       expect(@first_recipe.created_at).to eq '2023-02-10 03:46:44.223198'
     end
